9b14df19c63bcb2981487b0be05c745c0c1a9764
Elena Sokolova <>
2023-09-07 12:54:48 +0200

    fixed AllureListener

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -3,6 +3,7 @@ package helpers;
 import config.BrowserstackConfig;
 import org.aeonbits.owner.ConfigFactory;
 
+import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
@@ -14,6 +15,8 @@ public class Browserstack {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
+                .log().all()
+                .filter(withCustomTemplates())
                 .auth().basic(config.getUser(), config.getKey())
                 .when()
                 .get(url)
--- src/test/java/helpers/CustomAllureListener.java
+++ src/test/java/helpers/CustomAllureListener.java
@@ -1,9 +1,8 @@
-package in.reqres.helpers;
+package helpers;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 
 public class CustomAllureListener {
-
     private static final AllureRestAssured FILTER = new AllureRestAssured();
 
     public static AllureRestAssured withCustomTemplates() {
