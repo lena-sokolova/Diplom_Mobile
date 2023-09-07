2f049340c04d8146d52fc37508c43138557576c4
Elena Sokolova <>
2023-09-07 12:57:26 +0200

    refactor

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
-    public static String ENV = System.getProperty("env", "local");
+    public static String ENV = System.getProperty("env");
 
     @BeforeAll
     static void beforeAll() {
